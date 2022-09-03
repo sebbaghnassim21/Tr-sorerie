7c18acf1f4b86d867ac2c40d3ba35d5c24fe652e
frappe <frappe@ubuntu.vm>
2022-09-03 06:57:02 +0000

    firdt tresorerie

---
+++ tresorerie/tresorerie/doctype/compte/compte.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, nassim and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Compte', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ tresorerie/tresorerie/doctype/operations/operations.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, nassim and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class Operations(Document):
+	pass
